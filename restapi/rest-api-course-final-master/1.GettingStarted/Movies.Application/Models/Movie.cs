d426618e0a081b99b83d53739e4b343dcc753e06
jrneto <reato.neto@gmail.com>
2023-11-19 19:31:49 -0300

    Criação de filtro customizado na rota get (slug)

--- restapi/rest-api-course-final-master/1.GettingStarted/Movies.Application/Models/Movie.cs
+++ restapi/rest-api-course-final-master/1.GettingStarted/Movies.Application/Models/Movie.cs
@@ -1,13 +1,32 @@
-﻿namespace Movies.Application.Models
+﻿using System.Text.RegularExpressions;
+
+namespace Movies.Application.Models
 {
-    public class Movie
+    public partial class Movie
     {
         public required Guid Id { get; init; }
 
         public required string Title { get; init; }
 
+        public string Slug => GenereteSlug();
+
         public required int YearOfRelease { get; init; }
 
         public required List<string> Genres { get; init; } = new();
+
+        private string GenereteSlug()
+        {
+            //var sluggedTitle = Regex.Replace(Title, "[^0-9A-Za-z _-]", string.Empty)
+            //    .ToLower().Replace(" ", "-");
+
+            var sluggedTitle = SlugRegex().Replace(Title, string.Empty)
+                .ToLower().Replace(" ", "-");
+
+            return $"{sluggedTitle}-{YearOfRelease}";
+        }
+
+        [GeneratedRegex("[^0-9A-Za-z _-]", RegexOptions.NonBacktracking, 5)]
+        private static partial Regex SlugRegex();
+
     }
 }
