820af6c7b6dea5a024cd208fd797aad4fe55bd22
jrneto <reato.neto@gmail.com>
2023-12-08 10:23:51 -0300

    basic api versioning

--- restapi/rest-api-course-final-master/1.GettingStarted/Movies.Api/Controllers/V1/MoviesController.cs
+++ restapi/rest-api-course-final-master/1.GettingStarted/Movies.Api/Controllers/V1/MoviesController.cs
@@ -6,7 +6,7 @@ using Movies.Application.Services;
 using Movies.Contracts.Requests;
 using Movies.Contracts.Responses;
 
-namespace Movies.Api.Controllers
+namespace Movies.Api.Controllers.V1
 {
     [ApiController]
     public class MoviesController : ControllerBase
@@ -19,27 +19,27 @@ namespace Movies.Api.Controllers
         }
 
         [Authorize(AuthConstants.TrustedMemberPolicyName)]
-        [HttpPost(ApiEndpoints.Movies.Create)]
+        [HttpPost(ApiEndpoints.V1.Movies.Create)]
         public async Task<IActionResult> Create(
-            [FromBody]CreateMovieRequest request,
+            [FromBody] CreateMovieRequest request,
             CancellationToken token)
         {
             var movie = request.MapToMovie();
 
             await _movieService.CreateAsync(movie, token);
 
-            return CreatedAtAction(nameof(Get), new {idOrSlug = movie.Id}, movie);
+            return CreatedAtAction(nameof(Get), new { idOrSlug = movie.Id }, movie);
             //return Created($"/{ApiEndpoints.Movies.Create}/{movie.Id}", movie);
         }
 
-        [HttpGet(ApiEndpoints.Movies.Get)]
+        [HttpGet(ApiEndpoints.V1.Movies.Get)]
         public async Task<IActionResult> Get([FromRoute] string idOrSlug,
             [FromServices] LinkGenerator linkGenerator,
             CancellationToken token)
         {
             var userId = HttpContext.GetUserId();
 
-            var movie = Guid.TryParse(idOrSlug, out var id) 
+            var movie = Guid.TryParse(idOrSlug, out var id)
                 ? await _movieService.GetByIdAsync(id, userId, token)
                 : await _movieService.GetBySlugAsync(idOrSlug, userId, token);
 
@@ -54,7 +54,7 @@ namespace Movies.Api.Controllers
 
             response.Links.Add(new Link
             {
-                Href = linkGenerator.GetPathByAction(HttpContext, nameof(Get), values: new {idOrSlug = movie.Id}),
+                Href = linkGenerator.GetPathByAction(HttpContext, nameof(Get), values: new { idOrSlug = movie.Id }),
                 Rel = "self",
                 Type = "GET"
             });
@@ -76,7 +76,7 @@ namespace Movies.Api.Controllers
             return Ok(response);
         }
 
-        [HttpGet(ApiEndpoints.Movies.GetAll)]
+        [HttpGet(ApiEndpoints.V1.Movies.GetAll)]
         public async Task<IActionResult> GetAll(
             [FromQuery] GetAllMoviesRequest request,
             CancellationToken token)
@@ -95,7 +95,7 @@ namespace Movies.Api.Controllers
         }
 
         [Authorize(AuthConstants.TrustedMemberPolicyName)]
-        [HttpPut(ApiEndpoints.Movies.Update)]
+        [HttpPut(ApiEndpoints.V1.Movies.Update)]
         public async Task<IActionResult> Update(
             [FromRoute] Guid id,
             [FromBody] UpdateMovieRequest request,
@@ -118,7 +118,7 @@ namespace Movies.Api.Controllers
         }
 
         [Authorize(AuthConstants.AdminUserPolicyName)]
-        [HttpDelete(ApiEndpoints.Movies.Delete)]
+        [HttpDelete(ApiEndpoints.V1.Movies.Delete)]
         public async Task<IActionResult> Delete([FromRoute] Guid id,
             CancellationToken token)
         {
