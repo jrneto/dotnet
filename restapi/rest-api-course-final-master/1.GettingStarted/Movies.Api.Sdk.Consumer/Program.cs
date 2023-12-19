c97324646189c1d49bbb52a305b6576a6d8965bb
jrneto <reato.neto@gmail.com>
2023-12-19 08:29:55 -0300

    Criação de SDK com refit

---
+++ restapi/rest-api-course-final-master/1.GettingStarted/Movies.Api.Sdk.Consumer/Program.cs
@@ -0,0 +1,28 @@
+﻿using Movies.Api.Sdk;
+using Movies.Contracts.Requests;
+using Refit;
+using System.Text.Json;
+
+var moviesApi = RestService.For<IMoviesApi>("https://localhost:5001");
+
+var movie = await moviesApi.GetMovieAsync("6de0d777-06cf-427f-bb42-55f423268e0a");
+
+Console.WriteLine(JsonSerializer.Serialize(movie));
+
+var request = new GetAllMoviesRequest
+{
+    Title = null,
+    Year = null,
+    SortBy = null,
+    Page = 1,
+    PageSize = 3
+};
+
+var movies = await moviesApi.GetMoviesAsync(request);
+
+foreach (var movieResponse in movies.Items)
+{
+    Console.WriteLine(JsonSerializer.Serialize(movieResponse));
+}
+
+Console.ReadLine();
