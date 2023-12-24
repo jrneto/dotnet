09f442285d37aad6c90726b485e21c90c86fc991
jrneto <reato.neto@gmail.com>
2023-12-24 14:58:49 -0300

    Migrando API Version para minimal apis

--- restapi/rest-api-course-final-master/1.GettingStarted/Movies.Api/EndPoints/Movies/GetAllMovieEndPoint.cs
+++ restapi/rest-api-course-final-master/1.GettingStarted/Movies.Api/EndPoints/Movies/GetAllMovieEndPoint.cs
@@ -28,14 +28,43 @@ namespace Movies.Api.EndPoints.Movies
 
                 var moviesResponse = movies
                     .MapToResponse(
-                        request.Page.GetValueOrDefault(PagedRequest.DefaultPage), 
-                        request.PageSize.GetValueOrDefault(PagedRequest.DefaultPageSize), 
+                        request.Page.GetValueOrDefault(PagedRequest.DefaultPage),
+                        request.PageSize.GetValueOrDefault(PagedRequest.DefaultPageSize),
                         movieCount);
 
                 return TypedResults.Ok(moviesResponse);
             })
+            .WithName($"{Name}V1")
             .Produces<MoviesResponse>(StatusCodes.Status200OK)
-            .WithName(Name);
+            .WithApiVersionSet(ApiVersioning.VersionSet)
+            .HasApiVersion(1.0);
+
+            app.MapGet(ApiEndpoints.Movies.GetAll, async (
+                [AsParameters] GetAllMoviesRequest request,
+                IMovieService movieService,
+                HttpContext context,
+                CancellationToken token) =>
+            {
+                var userId = context.GetUserId();
+
+                var options = request.MapToOptions()
+                    .WithUser(userId);
+
+                var movies = await movieService.GetAllAsync(options, token);
+                var movieCount = await movieService.GetCountAsync(options.Title, options.YearOfRelease, token);
+
+                var moviesResponse = movies
+                    .MapToResponse(
+                        request.Page.GetValueOrDefault(PagedRequest.DefaultPage),
+                        request.PageSize.GetValueOrDefault(PagedRequest.DefaultPageSize),
+                        movieCount);
+
+                return TypedResults.Ok(moviesResponse);
+            })
+            .WithName($"{Name}V2")
+            .Produces<MoviesResponse>(StatusCodes.Status200OK)
+            .WithApiVersionSet(ApiVersioning.VersionSet)
+            .HasApiVersion(2.0);
 
             return app;
         }
